777d11cc4a3c955a11b6e6d56e7f1d3e6be8b19c
Md. Mowahibur Rahman Twake <twake.edu@gmail.com>
2023-12-31 12:02:16 +0600

    Add calculation

---
+++ src/Calculator.kt
@@ -0,0 +1,12 @@
+fun main() {
+    var num1 = readln()!!.toInt()
+    var num2 = readln()!!.toInt()
+    var addition = num1 + num2
+    var subtraction = num1 - num2
+    var multiplication = num1 * num2
+    var division = num1 / num2
+    println("Addition: $addition")
+    println("Subtraction: $subtraction")
+    println("Multiplication: $multiplication")
+    println("Division: $division")
+}
